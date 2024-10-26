03a35b9bac26b77741979480578f0933171ab6b1
dmhd6219 <svyatoslavsvyatkin@yandex.ru>
2024-10-27 01:03:33 +0300

    feat: add teachers page

--- src/components/router/teachers/page.tsx
+++ src/components/router/teachers/page.tsx
@@ -1,6 +1,7 @@
 import { Input } from '@/components/ui/input.tsx';
 import { IoSearchSharp } from 'react-icons/io5';
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar.tsx';
+
+import Teacher from '@/components/router/teachers/teacher.tsx';
 
 export default function Teachers() {
     return (
@@ -12,31 +13,8 @@ export default function Teachers() {
                 </div>
             </div>
 
-            <div className='w-full flex flex-col items-center gap-y-2 bg-bgColor p-4 rounded'>
-                <Avatar className='w-1/3 h-1/3'>
-                    <AvatarImage src='https://github.com/shadcn.png' />
-                    <AvatarFallback>CN</AvatarFallback>
-                </Avatar>
-                <span className='text-lg'>Морти Риков</span>
-                <span>
-                    Лидер компетенции JS, руководитель технологического Хаба Сбера в Казани с опытом
-                    в разработке 16 лет, преподаватель курса «Cross-platform developer on
-                    JavaScript» и опциональных модулей &#34;Electron&#34; и &#34;React-native&#34;
-                </span>
-            </div>
-
-            <div className='w-full flex flex-col items-center gap-y-2 bg-bgColor p-4 rounded'>
-                <Avatar className='w-1/3 h-1/3'>
-                    <AvatarImage src='https://github.com/shadcn.png' />
-                    <AvatarFallback>CN</AvatarFallback>
-                </Avatar>
-                <span className='text-lg'>Морти Риков</span>
-                <span>
-                    Лидер компетенции JS, руководитель технологического Хаба Сбера в Казани с опытом
-                    в разработке 16 лет, преподаватель курса «Cross-platform developer on
-                    JavaScript» и опциональных модулей &#34;Electron&#34; и &#34;React-native&#34;
-                </span>
-            </div>
+            <Teacher />
+            <Teacher />
         </div>
     );
 }
