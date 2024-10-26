6d3698fcc050a0abe76f55cb4e1eae103059231c
dmhd6219 <svyatoslavsvyatkin@yandex.ru>
2024-10-26 18:07:21 +0300

    feat: test main btn

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import { useTelegram } from '@/utils/contexts/telegram.tsx';
+import { Link } from 'react-router-dom';
 
 function App() {
     const telegram = useTelegram();
 
-    return <div>{JSON.stringify(telegram)}</div>;
+    return (
+        <div className='flex flex-col gap-y-4'>
+            {JSON.stringify(telegram)}
+            <Link to='/course/1'>Страница курса</Link>
+        </div>
+    );
 }
 
 export default App;
