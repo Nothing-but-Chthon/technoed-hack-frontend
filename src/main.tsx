ebb1b7a9d64284ff2a25453786ac554655aebd23
dmhd6219 <svyatoslavsvyatkin@yandex.ru>
2024-10-26 16:34:26 +0300

    init: add react riuter dom

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,19 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import { TelegramProvider } from '@/utils/contexts/telegram.tsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />
+    }
+]);
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <TelegramProvider>
-            <App />
+            <RouterProvider router={router} />
         </TelegramProvider>
     </StrictMode>
 );
