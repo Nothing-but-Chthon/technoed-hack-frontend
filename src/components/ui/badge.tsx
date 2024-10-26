552a653dfd751b17d50bb395f022c81250c2746c
dmhd6219 <svyatoslavsvyatkin@yandex.ru>
2024-10-26 22:46:06 +0300

    fix: badge color

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -9,7 +9,7 @@ const badgeVariants = cva(
         variants: {
             variant: {
                 default:
-                    'border-transparent bg-buttonColor text-primary-foreground hover:bg-primary/80',
+                    'border-transparent bg-buttonColor text-buttonTextColor hover:bg-primary/80',
                 secondary:
                     'border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80',
                 destructive:
